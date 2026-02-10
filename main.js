c69ecaaa667dc6b81e34249df85ae04cbf1eac6c
sumanpatra2000 <69432331+sumanpatra2000@users.noreply.github.com>
2026-02-09 21:26:47 -0600

    uploading new changes to lab 2

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /////// Variables Section ///////
-const myName = "Clint Tuttle"
+const myName = "Suman Patra"
 const fruits = ['apples', 'bananas', 'blueberries', 'strawberries', 'pineapples'];
 const car = [{make:"Toyota", model:"Prius", color:"Silver"},
              {make:"Chevy", model:"Bolt", color:"Blue"}, 
@@ -51,13 +51,13 @@ const result2Element = document.getElementById('result2');
 
 ////starter code////
 //update code below to using template literals (not concatenation) to pull in the variables myName, color, make, into output
-result2Element.innerHTML = "Hello my name is {yourname} and I drive a {White Honda Civic}";
+result2Element.innerHTML = `Hello my name is ${myName} and I drive a ${car[2].color}  ${car[2].make}  ${car[2].model}. Year is ${today.getFullYear()}`;
 
 
 //Footer Section
 const footerElement = document.getElementById("footer");
 //update code below to use template literals to pull in the myName and the current year
-footerElement.innerHTML = "&copy; Clint Tuttle 2024";
+footerElement.innerHTML = `&copy; ${myName} ${today.getFullYear()}`;
 
 
 
@@ -65,16 +65,16 @@ footerElement.innerHTML = "&copy; Clint Tuttle 2024";
 
 //////// PART 3 - Ternary Operator////////
 ////section 1 - basic if else statement
-if (loggedIn) {
-    const result3Element = document.getElementById('result3');
-    result3Element.innerHTML = "Welcome back!";
-} else {
-    const result3Element = document.getElementById('result3');
-    result3Element.innerHTML = "Please log in";
-}
+const result3Element = document.getElementById('result3');
 
-////Sample code.  You can uncomment this to see how simple a ternaly operator can be
-// loggedIn ? alert("welcome back") : alert("please log in");
+const conditonlogged=(loggedIn)?result3Element.innerHTML = "Welcome back!":result3Element.innerHTML = "Please log in";
+
+    
+    
+
+
+
+//loggedIn ? alert("welcome back") : alert("please log in");
 
 ////section 2 -ternary operator to update the result3Element
 
@@ -83,37 +83,57 @@ if (loggedIn) {
 
 
 //////// PART 4 - Arrow Functions////////
-function Greetings(name) {
-    return `Hello ${name}!`;
-};
+// function Greetings(name) {
+//     return `Hello ${name}!`;
+// };
 
-console.log(Greetings("Clint"));
+// console.log(Greetings("Clint"));
 
 //Make a copy of the above function and rename it GreetingsArrow() and then convert function to an arrow function here
  
+const GreetingsArrow =(name)=> {
+  return `Hello ${name}!`;
+};
 
+console.log(GreetingsArrow("Suman"));
 
 
 //////// PART 5 - Array methods////////
+// function printFruit() {
+
+//     let resultElement = document.getElementById("result5");
+
+//     resultElement.textContent = "";
+
+//     // Loop through the fruits array
+//     for (let i = 0; i < fruits.length; i++) {
+ 
+//         // Create a list item for each fruit
+//         let p = document.createElement("p");
+//         p.textContent = fruits[i];
+
+//         // Append the list item to the ordered list
+//         resultElement.appendChild(p);
+//     }
+
+//  }
+
 function printFruit() {
+  let resultElement = document.getElementById("result5");
+  resultElement.textContent = "";
 
-    let resultElement = document.getElementById("result5");
-
-    resultElement.textContent = "";
-
-    // Loop through the fruits array
-    for (let i = 0; i < fruits.length; i++) {
+  
+  fruits
+    .filter(fruit => fruit.toLowerCase().startsWith('b'))
+    .map(fruit => {
+      let p = document.createElement("p");
+      p.textContent = fruit;
+      return p;
+    })
+    .forEach(pElement => resultElement.appendChild(pElement));
+}
  
-        // Create a list item for each fruit
-        let p = document.createElement("p");
-        p.textContent = fruits[i];
 
-        // Append the list item to the ordered list
-        resultElement.appendChild(p);
-    }
-
- }
- 
  
  function clearText() {
     let resultElement = document.getElementById("result5");
@@ -127,19 +147,22 @@ function printFruit() {
  //////// PART6 - Destructuring////////
  myCar(car[0]);
 
- function myCar(car) {
-     var make = car.make;
-     var model = car.model;
-     var color = car.color;
+//  function myCar(car) {
+//      var make = car.make;
+//      var model = car.model;
+//      var color = car.color;
  
-     var message = `I drive a ${color} ${make} ${model}`;
+//      var message = `I drive a ${color} ${make} ${model}`;
    
-     document.getElementById("result6").textContent = message;
- }
+//      document.getElementById("result6").textContent = message;
+//  }
 
 //convert the myCar function to use destructuring
 
-
+function myCar({ make, model, color }) {
+  const message = `I drive a ${color} ${make} ${model}`;
+  document.getElementById("result6").textContent = message;
+}
 
 
 
@@ -164,35 +187,51 @@ const baseProduct = {
   // Merge basePrduct and Seasonal using spread operator and add lastUpdated property with a value of '2023-12-31'
   ///UPDATE THIS CODE
   const currentProduct = { 
-
+    ...baseProduct, 
+    ...holidayPromo, 
+    lastUpdated: '2023-12-31' 
 };
   
   //Check the result object in the console:
   console.log(currentProduct);
   //UPDATE THIS OUTPUT
-  document.getElementById("result7").textContent = `The .....`; 
+  document.getElementById("result7").textContent = `The ${currentProduct.name} are on sale for ${currentProduct.price} as of ${currentProduct.lastUpdated}`;
 
 
 
 
 
- //////// PART 8 - Try Catch////////
- function InputValidation() {
+  function InputValidation() {
     const message = document.getElementById("part8-p");
     message.innerHTML = "";
     
     let x = document.getElementById("part8-input").value;
     
     try { 
-  
+        
+        if (x.trim() == "") throw "is EMPTY";
+        
+        
+        if (isNaN(x)) throw "is NOT A NUMBER";
+        
+        
+        x = Number(x);
+
+        
+        if (x > 15) throw "is TOO HIGH";
+        if (x < 10) throw "is TOO LOW";
+
+        
+        message.innerHTML = "Success";
     }
     catch(err) {
-     } 
+        
+        message.innerHTML = "Input " + err;
+    } 
     finally {
-
+        
+        document.getElementById("part8-input").value = "";
     }
-
-    
-  }
+}
 
 
